5146eada5c3c0998d277a3a24fe05e6875ccaf00
Melym1337 <galymzhanexe@gmail.com>
2026-01-27 11:28:54 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+const students = [
+    // {
+    //     name: "Name Surname",
+    //     subject: "Математика",
+    //     certificate: "certificates/ivan_math.pdf"
+    // },
+    {
+        name: "Иван Петров",
+        subject: "Математика",
+        certificate: "certificates/ivan_math.pdf"
+    },
+    {
+        name: "Айгерим Нур",
+        subject: "Физика",
+        certificate: "certificates/aigerim_physics.pdf"
+    },
+    {
+        name: "Айгерим Нур",
+        subject: "Информатика",
+        certificate: "certificates/aigerim_it.pdf"
+    }
+];
